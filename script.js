06903f17bde6a2869dae9c0e7c85d9659a065d7e
Rachel Isaac <rachelisaac995@gmail.com>
2022-10-16 22:55:29 +0400

    styles: added css animations and hamburger menu

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+// Hamburger Menu
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
